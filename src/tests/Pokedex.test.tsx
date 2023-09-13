9d787e265a1f994b644b508aa788efecf0eebbd4
fabricioFalcao <fabriciohfd@gmail.com>
2023-09-13 18:04:32 -0300

    Testes da Pokedex com melhorias

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -16,19 +16,14 @@ describe('Testes do componente Pokedex', () => {
     const nextBtn = screen.getByRole('button', { name: /próximo pokémon/i });
     expect(nextBtn).toHaveTextContent('Próximo Pokémon');
 
-    // for (let index = 0; index < pokemonList.length; index += 1) {
-    //   screen.getByText(pokemonList[index].name);
+    for (let index = 0; index < pokemonList.length; index += 1) {
+      screen.getByText(pokemonList[index].name);
 
-    //   const cardsList = screen.getAllByRole('link', { name: /more details/i });
-    //   expect(cardsList.length).toBe(1);
+      const cardsList = screen.getAllByRole('link', { name: /more details/i });
+      expect(cardsList.length).toBe(1);
 
-    //   await user.click(nextBtn);
-    // }
-    screen.getByText(pokemonList[0].name);
-
-    await user.click(nextBtn);
-
-    screen.getByText(pokemonList[1].name);
+      await user.click(nextBtn);
+    }
   });
 
   test('Teste dos botões de filtro da Pokedex', async () => {
@@ -48,46 +43,26 @@ describe('Testes do componente Pokedex', () => {
     const filterBtnList = screen.getAllByTestId(typeId);
     expect(filterBtnList.length).toBe(typesList.length);
 
-    // for (let index = 0; index < typesList.length; index += 1) {
-    //   const currentType = typesList[index];
-    //   const pokemonsPerType = pokemonList
-    //     .filter((pokemon) => pokemon.type === currentType).length;
+    for (let index = 0; index < typesList.length; index += 1) {
+      const currentType = typesList[index];
+      const pokemonsPerType = pokemonList
+        .filter((pokemon) => pokemon.type === currentType).length;
 
-    //   const filterButton = screen.getByRole('button', { name: currentType });
+      const filterButton = screen.getByRole('button', { name: currentType });
 
-    //   await user.click(filterButton);
+      await user.click(filterButton);
 
-    //   expect(filterButton).toHaveAttribute('data-testid', typeId);
-    //   expect(allButton).not.toHaveAttribute('data-testid', typeId);
+      expect(filterButton).toHaveAttribute('data-testid', typeId);
+      expect(allButton).not.toHaveAttribute('data-testid', typeId);
 
-    //   for (let i = 0; i < pokemonsPerType; i += 1) {
-    //     await user.click(screen.getByRole('button', { name: /próximo pokémon/i }));
-    //     const cardType = screen.getByTestId('pokemon-type');
-    //     expect(cardType).toHaveTextContent(currentType);
-    //   }
-    // }
-
-    // await user.click(allButton);
-    // screen.getByText(pokemonList[0].name);
-
-    filterBtnList.forEach((button, index) => {
-      expect(button).toHaveTextContent(typesList[index]);
-    });
-
-    await user.click(filterBtnList[1]);
-
-    const pokemonName = screen.getByTestId('pokemon-name');
-    const nextButton = screen.getByRole('button', { name: 'Próximo Pokémon' });
-
-    expect(pokemonName).toHaveTextContent('Charmander');
-
-    await user.click(nextButton);
-    expect(pokemonName).toHaveTextContent('Rapidash');
-
-    await user.click(filterBtnList[2]);
-    expect(pokemonName).toHaveTextContent('Caterpie');
+      for (let i = 0; i < pokemonsPerType; i += 1) {
+        await user.click(screen.getByRole('button', { name: /próximo pokémon/i }));
+        const cardType = screen.getByTestId('pokemon-type');
+        expect(cardType).toHaveTextContent(currentType);
+      }
+    }
 
     await user.click(allButton);
-    expect(pokemonName).toHaveTextContent('Pikachu');
+    screen.getByText(pokemonList[0].name);
   });
 });
