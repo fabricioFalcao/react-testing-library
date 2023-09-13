7c52ad27762ee7f54a81e1c4dce2a301a38cdfdb
fabricioFalcao <fabriciohfd@gmail.com>
2023-09-13 09:00:30 -0300

    Requisito 4

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,17 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testes do componente Not Found', () => {
+  test('A página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    screen.getByRole('heading', { level: 2, name: 'Page requested not found' });
+  });
+
+  test('A página mostra a imagem com o texto alternativo', () => {
+    renderWithRouter(<NotFound />);
+
+    screen.getByRole('img', { name: /clefairy pushing buttons randomly with text i have no idea what i'm doing/i });
+  });
+});
